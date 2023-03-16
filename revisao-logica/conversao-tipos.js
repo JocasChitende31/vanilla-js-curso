e97a670ff1e4530a57c90f1c554e665027b7419d
JocasChitende31 <84420296+JocasChitende31@users.noreply.github.com>
2023-03-16 11:02:24 +0100

    adiciona aula de operdadores

---
+++ revisao-logica/conversao-tipos.js
@@ -0,0 +1,12 @@
+let n1 = 10;
+let n2 = "2";
+n2 = parseInt(n2);
+
+console.log(n1 + n2, typeof n1, typeof n2);
+
+// formas para converter string em numbers: parseFloat, parseInt e Number()
+
+n2 = 20;
+n2 = n2.toString(16);
+
+console.log(n2, typeof n2);
